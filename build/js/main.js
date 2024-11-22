88df577ea2e00ffd5db8a89f3f401ae2fb503bc4
Mari <mari@MacBook-Pro.local>
2024-11-22 16:03:16 +0200

    first

---
+++ build/js/main.js
@@ -0,0 +1,3 @@
+"use strict";
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiLCJzZWNvbmQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUNBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiIiwiIl19
